483ea95c730223fce4b5fcd74c934ca28df1abde
Hailey L <haileylan404@gmail.com>
2025-05-30 22:13:48 -0400

    f

--- index.js
+++ index.js
@@ -1,26 +1,88 @@
+//STARTING PAGE VARS
 const canvas = document.querySelector('.gameCanvas');
 const context = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 const startBg = document.querySelector('.startBg');
-const scene1Bg = document.querySelector('.scene1Bg');
 const playBtn = document.querySelector('.playBtn');
 const playSpan = document.querySelector('.playSpan');
 const playBtnBg = document.querySelector('.playBtnBg');
+//SCENE 1 VARS
+const scene1Bg = document.querySelector('.scene1Bg');
+const textbox = document.querySelector('.textbox');
+const textboxDiv = document.querySelector('.textboxDiv');
+const text = document.querySelector('.text');
+
+const fadeOverlay = document.querySelector('.fadeOverlay');
+const timeoutTime = 5000;
 
 window.onload = function(){
     context.drawImage(startBg, 0, 0, canvas.width, canvas.height);
     playBtn.style.display = 'block';
     playSpan.style.display = 'inline-block';
     playBtnBg.style.display = 'block';
-    playBtn.style.left = (canvas.width - 600) + 'px';
-    playBtn.style.top = (canvas.height / 2) + 'px';
+    playBtn.style.left = (canvas.width / 2) + 'px';
+    playBtn.style.top = (canvas.height / 1.73) + 'px';
 }
 playBtn.addEventListener('click', () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    playBtn.style.display = 'none';
-    playSpan.style.display = 'none';
-    playBtnBg.style.display = 'none';
-    context.drawImage(scene1Bg, 0, 0, canvas.width, canvas.height);
-    console.log('hi')
+    fadeOverlay.style.display = 'block';
+    fadeOverlay.style.opacity = '1';
+
+    setTimeout(() => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        playBtn.style.display = 'none';
+        playSpan.style.display = 'none';
+        playBtnBg.style.display = 'none';
+        context.drawImage(scene1Bg, 0, 0, canvas.width, canvas.height);
+        scene1();
+
+        setTimeout(() => {
+            fadeOverlay.style.opacity = '.5';
+        }, 400)
+
+        fadeOverlay.style.opacity = '0';
+
+        setTimeout(() => {
+            fadeOverlay.style.display = 'none';
+        }, 400)
+    }, 200);
+    
 })
+function scene1(){
+    textboxDiv.style.display = 'block';
+    textbox.style.display = 'block';
+    text.style.display = 'inline-block';
+    textboxDiv.style.left = ((canvas.width - 500) / 2) + 'px';
+    textboxDiv.style.top = ((canvas.height) / 1.35) + 'px';
+    setTimeout(()=>{
+        text.innerText = "Her name was Catricia Calligan, and she owned a flower shop in mid Paw City. She was an active member of the Cathatten country club.";
+    }, timeoutTime);
+    setTimeout(()=>{
+        text.innerText = "We are about to enter her house, where the crime has been reported by her neighbor, Pawshley Catsmith."
+    }, timeoutTime * 2);
+}
+function scene1(){
+    textboxDiv.style.display = 'block';
+    textbox.style.display = 'block';
+    text.style.display = 'inline-block';
+    textboxDiv.style.left = ((canvas.width - 500) / 2) + 'px';
+    textboxDiv.style.top = ((canvas.height) / 1.35) + 'px';
+    setTimeout(()=>{
+        text.innerText = "Her name was Catricia Calligan, and she owned a flower shop in mid Paw City. She was an active member of the Cathatten country club.";
+    }, timeoutTime);
+    setTimeout(()=>{
+        text.innerText = "We are about to enter her house, where the crime has been reported by her neighbor, Pawshley Catsmith."
+    }, timeoutTime * 2);
+}
+function typeWriter(element, text, speed = 50, callback = null) {
+    let i = 0;
+    element.innerText = "";
+    const interval = setInterval(() => {
+        element.innerText += text.charAt(i);
+        i++;
+        if (i >= text.length) {
+            clearInterval(interval);
+            if (callback) callback();
+        }
+    }, speed);
+}
